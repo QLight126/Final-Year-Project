3b089bcb3fb73253c7121e98babd28963d98fe45
QLight126 <43591730+QLight126@users.noreply.github.com>
2020-11-20 21:35:36 +0800

    minor fix

--- Assets/Execute.cs
+++ Assets/Execute.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Execute : MonoBehaviour
 {
     public void Scene1()
     {
-        Execute.LoadScene("output");
+        SceneManager.LoadScene("output");
     }
 
 }
