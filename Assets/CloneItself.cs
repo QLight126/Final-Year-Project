5db43ee4111ba59dcc5042368b4f19ebc3023d4f
QLight126 <43591730+QLight126@users.noreply.github.com>
2020-12-11 22:04:07 +0800

    working on instantiate

---
+++ Assets/CloneItself.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneItself : MonoBehaviour
+{
+    public GameObject original;
+
+    public void clone()
+    {
+        GameObject clones = Instantiate(original, transform.position, transform.rotation);
+    }
+   
+}
--- Assets/Execute.cs
+++ Assets/Execute.cs
@@ -1,14 +1,17 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Execute : MonoBehaviour
 {
+    public Camera ProgramCamera;
+    public Camera OutputCamera;
+
     public void Scene1()
     {
-        SceneManager.LoadScene("output");
+        //SceneManager.LoadScene("output");
+        ProgramCamera.enabled = false;
+        OutputCamera.enabled = true;
     }
-
 }
