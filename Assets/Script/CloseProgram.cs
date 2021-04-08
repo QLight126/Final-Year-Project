935db08902cde7a554f7873d122447c508734fbb
QLight126 <43591730+QLight126@users.noreply.github.com>
2021-04-09 02:02:17 +0800

    add quit button

---
+++ Assets/Script/CloseProgram.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseProgram : MonoBehaviour
+{
+    // Close the program when the button is clicked.
+    public void closeProgram()
+    {
+        Application.Quit();
+    }
+}
